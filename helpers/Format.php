4e7026a249da1ec35fab490f49f6c7a98286240f
Sohana4567 <47296277+Sohana4567@users.noreply.github.com>
2019-10-04 18:16:21 +0530

    Add files via upload

---
+++ helpers/Format.php
@@ -0,0 +1,12 @@
+<?php
+
+class Format{
+    public function textShorten($text,$limit = 400){
+        $text = $text. " ";
+        $text = substr($text,0,$limit);
+        $text = substr($text,0,strrpos($text,' '));
+        $text=$text.".....";
+        return $text;
+
+    }
+}
